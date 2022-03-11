f77ce4fe25a89522148a985ce20ec5386ec20cec
Mohammad Sajib Hossen <101424447+YourBongGuy@users.noreply.github.com>
2022-03-12 01:15:57 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // nav styling
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     var topnav = document.getElementById('navbar');
-    topnav.classList.toggle('sticky', window.scrollY>20);
+    topnav.classList.toggle('sticky', window.scrollY > 20);
 })
 
 // responsive menu styling
@@ -18,27 +18,28 @@ menuShow.onclick = function(){
 
 // return to top styling
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     var returnTotop = document.getElementById('rtt');
     returnTotop.classList.toggle('rtt', window.scrollY > 500);
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
+        autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             },
@@ -46,16 +47,16 @@ $(document).ready(function(){
         }
     });
     let typed = new Typed('.typing', {
-        strings: ['Developer.', 'Freelancer.','YouTuber.', 'Bloger.', 'Designer.'],
+        strings: ['Developer.', 'Freelancer.', 'YouTuber.', 'Bloger.', 'Designer.'],
         typeSpeed: 100,
         backSpeed: 70,
         loop: true
-    
+
     });
 });
 
 let typed = new Typed('.typing-two', {
-    strings: ['Developer.', 'Freelancer.','YouTuber.', 'Bloger.', 'Designer.'],
+    strings: ['Developer.', 'Freelancer.', 'YouTuber.', 'Bloger.', 'Designer.'],
     typeSpeed: 100,
     backSpeed: 70,
     loop: true
@@ -63,3 +64,38 @@ let typed = new Typed('.typing-two', {
 });
 // typing animation 
 
+// social share
+$(document).ready(function () {
+    $('.facebook').mouseover(function () {
+        $('.f').slideDown('slow');
+    });
+    $('.facebook').mouseleave(function () {
+        $('.f').slideUp('fast');
+    });
+    $('.youtube').mouseover(function () {
+        $('.y').slideDown("slow");
+    });
+    $('.youtube').mouseleave(function () {
+        $('.y').slideUp('fast');
+    });
+
+    $('.twitter').mouseover(function () {
+        $('.t').slideDown("slow");
+    });
+    $('.twitter').mouseleave(function () {
+        $('.t').slideUp('fast');
+    });
+
+    $('.instagram').mouseover(function () {
+        $('.i').slideDown("slow");
+    });
+    $('.instagram').mouseleave(function () {
+        $('.i').slideUp('fast');
+    });
+    $('.whatsapp').mouseover(function () {
+        $('.w').slideDown("slow");
+    });
+    $('.whatsapp').mouseleave(function () {
+        $('.w').slideUp('fast');
+    });
+});
